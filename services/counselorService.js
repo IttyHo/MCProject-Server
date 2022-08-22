2c29e92af1ca2506c6884f410724dd629d27634b
IttyHo <h3241788@gmail.com>
2022-08-22 14:44:59 +0300

    function getCounselorDetaileByOfficeId

--- services/counselorService.js
+++ services/counselorService.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { getCustFromSQL } = require('./dbService');
 const {sqlConfig}=require('./dbService'); 
 const sql = require("mssql/msnodesqlv8");
-
+const officeId=null;
 
 router.get('/getCounselor', function (req, res, next) {
        
@@ -26,6 +26,33 @@ router.get('/getCounselor', function (req, res, next) {
       return  sql.connect(sqlConfig).then(pool => {
             return pool.request()
                 .execute('spGetCounselors')
+        })
+        
+      }
+
+      router.get('/getCounselorDetaileByOfficeId', function (req, res, next) {
+       const {officeId}=req.query
+       this.officeId=officeId;
+       console.log(this.officeId);
+  
+        try {
+            getCounselorDetaileByOfficeId().then(({recordset}) => {
+              res.send(recordset) ;
+                }).catch(err => {
+                   console.log( err);
+              }) 
+          }
+      catch (err){
+          console.log(err);
+          res.send("err");
+          }
+      })
+      
+      function getCounselorDetaileByOfficeId(){
+      return  sql.connect(sqlConfig).then(pool => {
+            return pool.request()
+            .input('OfficeId', sql.Int, this.officeId)
+            .execute('spGetCounselorDetaileByOfficeId')
         })
         
       }
