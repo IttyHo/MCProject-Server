01191fa553bea2f294e5564bb6279e8943f84d9a
IttyHo <h3241788@gmail.com>
2022-09-19 16:20:13 +0300

    קוטרול שיפט f

--- services/counselorService.js
+++ services/counselorService.js
@@ -1,165 +1,164 @@
 const express = require('express');
 const router = express.Router();
 const { getCustFromSQL } = require('./dbService');
-const {sqlConfig}=require('./dbService'); 
+const { sqlConfig } = require('./dbService');
 const sql = require("mssql/msnodesqlv8");
 
-router.get('/getCounselorOfficeType', function (req, res, next) {    
+router.get('/getCounselorOfficeType', function (req, res, next) {
   // const counselorTypeReq = req.query.counselorType;
-   try {
-       getCounselorOfficeType().then(({recordset}) => {
-         res.send(recordset) ;
-           }).catch(err => {
-              console.log( err);
-         }) 
-     }
- catch (err){
-     console.log(err);
-     res.send("err");
-     }
- })
- function getCounselorOfficeType(){
-  return  sql.connect(sqlConfig).then(pool => {
-        return pool.request()
-            .execute('spGetCounselorOfficeType')
+  try {
+    getCounselorOfficeType().then(({ recordset }) => {
+      res.send(recordset);
+    }).catch(err => {
+      console.log(err);
     })
-    
   }
-router.get('/getCounselor', function (req, res, next) {    
-       const counselorTypeReq = req.query.counselorType;
-        try {
-            getCounselor().then(({recordset}) => {
+  catch (err) {
+    console.log(err);
+    res.send("err");
+  }
+})
+function getCounselorOfficeType() {
+  return sql.connect(sqlConfig).then(pool => {
+    return pool.request()
+      .execute('spGetCounselorOfficeType')
+  })
 
-            if(counselorTypeReq){
-                recordset = recordset.filter(({CounselorOfficeType}) => counselorTypeReq===CounselorOfficeType );
-            } 
-              res.send(recordset) ;
-                }).catch(err => {
-                   console.log( err);
-              }) 
-          }
-      catch (err){
-          console.log(err);
-          res.send("err");
-          }
-      })
-      
-      function getCounselor(){
-      return  sql.connect(sqlConfig).then(pool => {
-            return pool.request()
-                .execute('spGetCounselors')
-        })
-        
+}
+router.get('/getCounselor', function (req, res, next) {
+  const counselorTypeReq = req.query.counselorType;
+  try {
+    getCounselor().then(({ recordset }) => {
+
+      if (counselorTypeReq) {
+        recordset = recordset.filter(({ CounselorOfficeType }) => counselorTypeReq === CounselorOfficeType);
       }
+      res.send(recordset);
+    }).catch(err => {
+      console.log(err);
+    })
+  }
+  catch (err) {
+    console.log(err);
+    res.send("err");
+  }
+})
 
-      router.get('/getCounselorDetaileByOfficeId', function (req, res, next) {
-       const {officeId}=req.query
-  
-        try {
-            getCounselorDetaileByOfficeId(officeId).then(({recordset}) => {
-              res.send(recordset) ;
-                }).catch(err => {
-                   console.log( err);
-              }) 
-          }
-      catch (err){
-          console.log(err);
-          res.send("err");
-          }
-      })
-      
-      function getCounselorDetaileByOfficeId(officeId){
-      return  sql.connect(sqlConfig).then(pool => {
-            return pool.request()
-            .input('OfficeId', sql.Int, officeId)
-            .execute('spGetCounselorDetaileByOfficeId')
-        })
-        
-      }
+function getCounselor() {
+  return sql.connect(sqlConfig).then(pool => {
+    return pool.request()
+      .execute('spGetCounselors')
+  })
 
-      router.get('/getCounselorsOfficeByOfficeId', function (req, res, next) {
-        const {officeId}=req.query
-   
-         try {
-             getCounselorsOfficeByOfficeId(officeId).then(({recordset}) => {
-               res.send(recordset) ;
-                 }).catch(err => {
-                    console.log( err);
-               }) 
-           }
-       catch (err){
-           console.log(err);
-           res.send("err");
-           }
-       })
-       
-       function getCounselorsOfficeByOfficeId(officeId){
-       return  sql.connect(sqlConfig).then(pool => {
-             return pool.request()
-             .input('OfficeId', sql.Int, officeId)
-             .execute('spGetCounselorsOfficeByOfficeId')
-         })
-         
-       }
+}
 
-       router.get('/getCounselorOfficeByProjectId', function (req, res, next) {
-        const {projectId}=req.query
-        
-         try {
-             getCounselorOfficeByProjectId(projectId).then(({recordset}) => {
-               res.send(recordset) ;
-                 }).catch(err => {
-                    console.log( err);
-               }) 
-           }
-       catch (err){
-           console.log(err);
-           res.send("err");
-           }
-       })
-       
-       function getCounselorOfficeByProjectId(projectId){
-       return  sql.connect(sqlConfig).then(pool => {
-             return pool.request()
-             .input('ProjectId', sql.Int,projectId)
-             .execute('spGetCounselorOfficeByProjectId')
-         })
-         
-       }
+router.get('/getCounselorDetaileByOfficeId', function (req, res, next) {
+  const { officeId } = req.query
 
-       router.post('/addCounselor', function (req, res, next) {
-        try {
-          const counselor = req.body;
-          addCounselor(counselor).then(() => {
-              res.send(true) ;
-                }).catch(err => {
-                   console.log( err);
-                   res.send(false) ;
-              }) 
-          }
-      catch (err){
-          console.log(err);
-          res.send("err");
-          }
-      })
-      function addCounselor(counselor){
-        return  sql.connect(sqlConfig).then(pool => {
-            const {CounselorOfficeName,CounselorOfficeAdress,CounselorOfficePhone,CounselorOfficeType,CounselorOfficeManager,CounselorOfficeManagerPhone,CounselorOfficeManagerMail,CounselorOfficeMainSecretary,CounselorOfficeMainSecretaryPhone,CounselorOfficeMainSecretaryMail} = counselor;
-            return pool.request()
-            .input('CounselorOfficeName', sql.NVarChar, CounselorOfficeName)
-            .input('CounselorOfficeAdress', sql.NVarChar, CounselorOfficeAdress)
-            .input('CounselorOfficePhone', sql.NVarChar, CounselorOfficePhone)
-            .input('CounselorOfficeType', sql.Int, CounselorOfficeType)
-            .input('CounselorOfficeManager', sql.NVarChar, CounselorOfficeManager)
-            .input('CounselorOfficeManagerPhone', sql.NVarChar, CounselorOfficeManagerPhone)
-            .input('CounselorOfficeManagerMail', sql.NVarChar, CounselorOfficeManagerMail)
-            .input('CounselorOfficeMainSecretary', sql.NVarChar, CounselorOfficeMainSecretary)
-            .input('CounselorOfficeMainSecretaryPhone', sql.NVarChar, CounselorOfficeMainSecretaryPhone)
-            .input('CounselorOfficeMainSecretaryMail', sql.NVarChar, CounselorOfficeMainSecretaryMail)
+  try {
+    getCounselorDetaileByOfficeId(officeId).then(({ recordset }) => {
+      res.send(recordset);
+    }).catch(err => {
+      console.log(err);
+    })
+  }
+  catch (err) {
+    console.log(err);
+    res.send("err");
+  }
+})
 
-            .execute('spAddCounselor')
-        })
-      }
+function getCounselorDetaileByOfficeId(officeId) {
+  return sql.connect(sqlConfig).then(pool => {
+    return pool.request()
+      .input('OfficeId', sql.Int, officeId)
+      .execute('spGetCounselorDetaileByOfficeId')
+  })
+}
+router.get('/getCounselorsOfficeByOfficeId', function (req, res, next) {
+  const { officeId } = req.query
+
+  try {
+    getCounselorsOfficeByOfficeId(officeId).then(({ recordset }) => {
+      res.send(recordset);
+    }).catch(err => {
+      console.log(err);
+    })
+  }
+  catch (err) {
+    console.log(err);
+    res.send("err");
+  }
+})
+
+function getCounselorsOfficeByOfficeId(officeId) {
+  return sql.connect(sqlConfig).then(pool => {
+    return pool.request()
+      .input('OfficeId', sql.Int, officeId)
+      .execute('spGetCounselorsOfficeByOfficeId')
+  })
+
+}
+
+router.get('/getCounselorOfficeByProjectId', function (req, res, next) {
+  const { projectId } = req.query
+
+  try {
+    getCounselorOfficeByProjectId(projectId).then(({ recordset }) => {
+      res.send(recordset);
+    }).catch(err => {
+      console.log(err);
+    })
+  }
+  catch (err) {
+    console.log(err);
+    res.send("err");
+  }
+})
+
+function getCounselorOfficeByProjectId(projectId) {
+  return sql.connect(sqlConfig).then(pool => {
+    return pool.request()
+      .input('ProjectId', sql.Int, projectId)
+      .execute('spGetCounselorOfficeByProjectId')
+  })
+
+}
+
+router.post('/addCounselor', function (req, res, next) {
+  try {
+    const counselor = req.body;
+    console.log(counselor);
+    addCounselor(counselor).then(() => {
+      res.send(true);
+    }).catch(err => {
+      console.log(err);
+      res.send(false);
+    })
+  }
+  catch (err) {
+    console.log(err);
+    res.send("err");
+  }
+})
+function addCounselor(counselor) {
+  return sql.connect(sqlConfig).then(pool => {
+    const { CounselorOfficeName, CounselorOfficeAdress, CounselorOfficePhone, CounselorOfficeType, CounselorOfficeManager, CounselorOfficeManagerPhone, CounselorOfficeManagerMail, CounselorOfficeMainSecretary, CounselorOfficeMainSecretaryPhone, CounselorOfficeMainSecretaryMail } = counselor;
+    return pool.request()
+      .input('CounselorOfficeName', sql.NVarChar, CounselorOfficeName)
+      .input('CounselorOfficeAdress', sql.NVarChar, CounselorOfficeAdress)
+      .input('CounselorOfficePhone', sql.NVarChar, CounselorOfficePhone)
+      .input('CounselorOfficeType', sql.Int, CounselorOfficeType)
+      .input('CounselorOfficeManager', sql.NVarChar, CounselorOfficeManager)
+      .input('CounselorOfficeManagerPhone', sql.NVarChar, CounselorOfficeManagerPhone)
+      .input('CounselorOfficeManagerMail', sql.NVarChar, CounselorOfficeManagerMail)
+      .input('CounselorOfficeMainSecretary', sql.NVarChar, CounselorOfficeMainSecretary)
+      .input('CounselorOfficeMainSecretaryPhone', sql.NVarChar, CounselorOfficeMainSecretaryPhone)
+      .input('CounselorOfficeMainSecretaryMail', sql.NVarChar, CounselorOfficeMainSecretaryMail)
+
+      .execute('spAddCounselor')
+  })
+}
 module.exports = router
 
 
